e69fbb9f145aa28fccfe89ad60f3eebc0cc10dff
bobbyamali <78933927+bobbyamali@users.noreply.github.com>
2021-02-11 20:09:18 -0100

    Add files via upload

---
+++ AMALI BOBBY/Serial monitor/serial_monitor_code/serial_monitor_code.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is AMALI BOBBY, 16CJ020695");
+  delay(3000);
+}
